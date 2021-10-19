b95faf04986d832f1fd526c2769c544d36204aa3
Kip Cole <kipcole9@gmail.com>
2021-10-20 06:16:35 +0800

    Release 1.2.0-rc.0

--- mix.exs
+++ mix.exs
@@ -39,9 +39,9 @@ defmodule Cldr.LocaleDisplay.MixProject do
 
   defp deps do
     [
-      {:ex_cldr, "~> 2.24-rc", override: true},
+      {:ex_cldr, "~> 2.24-rc"},
       {:ex_cldr_currencies, "~> 2.12-rc"},
-      {:ex_cldr_territories, "~> 2.2"},
+      {:ex_cldr_territories, "~> 2.3-rc"},
       {:jason, "~> 1.0", optional: true},
       {:ex_doc, "~> 0.18", onley: [:dev, :release], runtime: false, optional: true},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
